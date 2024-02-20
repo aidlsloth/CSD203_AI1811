2df72940ee3f372f001ef5b63e82e20d2f17eb21
aidlsloth <145555770+aidlsloth@users.noreply.github.com>
2024-02-20 10:05:24 +0700

    Update main.py

--- ASM1/main.py
+++ ASM1/main.py
@@ -1,3 +1,31 @@
 from mylist import *
 from book import * 
+from AddBook import *
+from DeleteBook import *
+from ReturnBook import *
+from ViewBooks import *
+from BorrowedBook import * 
 
+print("1. Add Book")
+print("2. Delete Book")
+print("3. Borrow Book")
+print("4. Return Book")
+print("5. View Book")
+choice = int(input("Selection 1 -> 5"))
+print("OUTPUT")
+
+if choice == 1:
+    newbook = input("Book's info")
+    AddBook(newbook)
+
+elif choice == 2:
+    DeleteBook()
+
+elif choice == 3:
+    BorrowedBook.Borrow()
+
+elif choice == 4:
+    ReturnBook()
+
+elif choice == 5:
+    ViewBooks()
